85298da02b366326d5c69b6898125545db8eab01
kostiask <56359519+kostiask@users.noreply.github.com>
2021-06-02 16:40:21 +0200

    11

--- project_django/lab_project/account/views.py
+++ project_django/lab_project/account/views.py
@@ -113,20 +113,21 @@ def editDataProfile(request, user_id):
     user = User.objects.get(pk=user_id)
     profile = Profile.objects.get(user_id=user_id)
     # form_change_password = ChangePassword()
-    form_edit_user_data = EditUserForm(request.POST, request.FILES)
+    form_edit_user_data = EditUserForm(request.POST)
     form_edit_phone = EditNumberOfPhone()
     # form_change_password = ChangePassword()
     form_change_password = PasswordChangeForm(user=request.user)
     photo_form = ProfileForm()
     if form_edit_user_data.is_valid():
-        user.first_name =  form_edit_user_data.cleaned_data['first_name']
+        print('dddddddddddddddddddddddddddddddddddddddddddd')
+        user.first_name = form_edit_user_data.cleaned_data['first_name']
         user.last_name = form_edit_user_data.cleaned_data['last_name']
         profile.birthday = form_edit_user_data.cleaned_data['birthday']
-        profile.photo = form_edit_user_data.cleaned_data['photo']
+        # profile.photo = form_edit_user_data.cleaned_data['photo']
         user.save()
         profile.save()
     else:
-        pass
+        print('dsffffffffffffffffffffffffffffffffffffffffffff')
     context = {
         'user': user,
         'profile': profile,
